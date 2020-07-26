a019c975c14dc8d3909ab0971411a0eb88e08223
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-07-26 17:03:44 -0300

    create Button & add Button

--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -1,6 +1,8 @@
 import styled, { css } from 'styled-components';
 import { LocationOn, Cake  } from "../../styles/Icons";
 
+import Button from '../Button';
+
 
 export const Container = styled.div`
   display: flex;
@@ -77,6 +79,21 @@ export const ProfileData = styled.div`
       }
     }
 
+  }
+`;
+
+export const EditButton = styled(Button)`
+  position: absolute;
+  top: 2vw;
+  right: 7px;
+
+  padding: 4px 16px;
+  font-size: 13px;
+
+  @media(min-width: 320px) {
+    top: 10;
+    padding: 10px 19px;
+    font-size: 15px;
   }
 `;
 
