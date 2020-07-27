71274d49e23b165344639cbf9942d6b2568a4a59
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-07-27 14:08:49 -0300

    create FollowSuggestion

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import List from '../List';
+import FollowSuggestion from '../FollowSuggestion';
 import {
   Container,
   SearchWrapper,
@@ -21,9 +22,18 @@ const SideBar: React.FC = () => {
         <List
           title="Talvez você curta"
           elements={[
-            <h1>test</h1>,
-            <h1>test</h1>,
-            <h1>test</h1>,
+            <FollowSuggestion
+              name="Karen Sabino"
+              nickname="@karensabino"
+            />,
+            <FollowSuggestion
+              name="Keyla Dutra"
+              nickname="@keyladutra"
+            />,
+            <FollowSuggestion
+              name="Roosevelt Souza"
+              nickname="@rooseveltssf"
+            />,
           ]}
         />
         <List
